71c302569877b0374c7bd12f331f6283b6d183e7
TJ Draper <tj@buzzingpixel.com>
2019-01-01 15:37:00 -0600

    Add twig function for throwing http error

---
+++ src/HttpTwigExtension.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace corbomite\http;
+
+use Twig_Function;
+use Twig_Extension;
+use corbomite\http\exceptions\Http404Exception;
+use corbomite\http\exceptions\Http500Exception;
+
+class HttpTwigExtension extends Twig_Extension
+{
+    public function getFunctions(): array
+    {
+        return [new Twig_Function('throwHttpError', [$this, 'throwHttpError'])];
+    }
+
+    /**
+     * @throws Http404Exception
+     * @throws Http500Exception
+     */
+    public function throwHttpError(int $code = 404, string $msg = ''): void
+    {
+        if ($code === 404) {
+            throw new Http404Exception($msg);
+        }
+
+        throw new Http500Exception($msg);
+    }
+}
