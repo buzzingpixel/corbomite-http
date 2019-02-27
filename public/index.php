58dbf645c4240e000e7e7e41610f108f76476ea3
TJ Draper <tj@buzzingpixel.com>
2019-02-27 08:46:17 -0600

    Update dependency on corbomite-config-collector to 1.1 so we don't need APP_BASE_PATH defined

--- public/index.php
+++ public/index.php
@@ -16,9 +16,6 @@ use corbomite\http\Kernel;
 
 putenv('DEV_MODE=true');
 
-define('APP_BASE_PATH', dirname(__DIR__));
-define('CSRF_EXEMPT_SEGMENTS', 'thing');
-
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 Di::get(Kernel::class)();
