71eaf6da9e675f70262ecafebc3eebfc29b9a50d
TJ Draper <tj@buzzingpixel.com>
2019-04-02 22:08:21 -0500

    Switch to Relay 2.0.0 official and final release (finally)

--- public/index.php
+++ public/index.php
@@ -17,5 +17,6 @@ use corbomite\http\Kernel;
 putenv('DEV_MODE=true');
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
+require_once dirname(__DIR__) . '/devMode.php';
 
 Di::get(Kernel::class)();
